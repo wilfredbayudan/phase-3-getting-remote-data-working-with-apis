252e67701e1457214197500ba81e33e535ee1882
Wilfred <wilfredbayudan@gmail.com>
2021-11-08 21:58:08 -1000

    commit tree 90ecfd9f2f6097f4f6d01bc4ecb9469c7e851ffa

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -10,9 +10,21 @@ class GetPrograms
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
+    
+  end
+
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]
+      end
   end
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
